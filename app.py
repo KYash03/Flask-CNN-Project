509d80c44333f972a587a5117304e2d48429268f
YASH KUMAR <kyash03@student.ubc.ca>
2023-08-28 00:05:13 +0530

    completed main_page_predict

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
-import os
 import numpy as np
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array, load_img
+
+
+app = Flask(__name__)
+model = load_model("static/x-ray_best_model.keras")
+
+
+@app.route("/", methods=["GET"])
+def main_page():
+    return render_template("index.html")
+
+
+@app.route("/", methods=["POST"])
+def main_page_predict():
+    image_file = request.files['image-file']
+    image_path = "./images/" + image_file.filename
+    image_file.save(image_path)
+
+    image = load_img(image_path,
+                     target_size=(64, 64))
+    image_array = img_to_array(image)
+    image_array = image_array.reshape((1, ) + image_array.shape)
+    image_array = image_array / 255.0
+    prediction = model.predict(image_array)
+    class_names = ["normal", "pneumonia"]
+    return render_template("index.html", prediction=class_names[np.argmax(prediction[0])])
+
+
+app.run(debug=True)
